8ca98c1af2791350bee0db5b51a0a9312eb53157
lizehao <lizehao@hupu.com>
2019-11-20 18:18:23 +0800

    feat:新增Channel代码

--- src/TestChannel.java
+++ src/TestChannel.java
@@ -1,12 +1,18 @@
+import org.junit.Test;
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class TestChannel {
 
-    public static void main(String[] args) {
+    @Test
+    public void test1() {
         FileInputStream fis = null;
         FileOutputStream fos = null;
         FileChannel inChannel = null;
@@ -55,5 +61,27 @@ public class TestChannel {
                 }
             }
         }
+    }
+
+    @Test
+    public void test2() throws Exception{
+        FileChannel inChannel = FileChannel.open(Paths.get("/Users/lizehao/IdeaProject/NIOdemo/src/a.txt"), StandardOpenOption.READ);
+        FileChannel outChannel = FileChannel.open(Paths.get("/Users/lizehao/IdeaProject/NIOdemo/src/c.txt"), StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+
+        MappedByteBuffer inMappedBuf = inChannel.map(FileChannel.MapMode.READ_ONLY, 0, inChannel.size());
+        MappedByteBuffer outMappedBuf = outChannel.map(FileChannel.MapMode.READ_WRITE,0, inChannel.size());
+
+        byte[] dst = new byte[inMappedBuf.limit()];
+        inMappedBuf.get(dst);
+        outMappedBuf.put(dst);
+    }
+
+    @Test
+    public void test3() throws Exception{
+        FileChannel inChannel = FileChannel.open(Paths.get("/Users/lizehao/IdeaProject/NIOdemo/src/a.txt"), StandardOpenOption.READ);
+        FileChannel outChannel = FileChannel.open(Paths.get("/Users/lizehao/IdeaProject/NIOdemo/src/d.txt"), StandardOpenOption.WRITE, StandardOpenOption.READ);
+        inChannel.transferTo(0, inChannel.size(), outChannel);
+        inChannel.close();
+        outChannel.close();
     }
 }

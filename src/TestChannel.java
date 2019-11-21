417631378123b0d40ce81ad398dd9025d97812d6
lizehao <lizehao@hupu.com>
2019-11-21 10:28:06 +0800

    fix:修复昨天第二个channel报错的问题

--- src/TestChannel.java
+++ src/TestChannel.java
@@ -12,64 +12,38 @@ import java.nio.file.StandardOpenOption;
 public class TestChannel {
 
     @Test
-    public void test1() {
-        FileInputStream fis = null;
-        FileOutputStream fos = null;
-        FileChannel inChannel = null;
-        FileChannel outChannel = null;
-        ByteBuffer byteBuffer = null;
-        try {
-            fis = new FileInputStream("/Users/lizehao/IdeaProject/NIOdemo/src/a.txt");
-            fos = new FileOutputStream("/Users/lizehao/IdeaProject/NIOdemo/src/b.txt");
-            inChannel = fis.getChannel();
-            outChannel = fos.getChannel();
-            byteBuffer = ByteBuffer.allocate(1024);
-            while (inChannel.read(byteBuffer) != -1) {
-                byteBuffer.flip();
-                outChannel.write(byteBuffer);
-                byteBuffer.clear();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (fos != null) {
-                try {
-                    fos.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (fis != null) {
-                try {
-                    fis.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (outChannel != null) {
-                try {
-                    outChannel.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (inChannel != null) {
-                try {
-                    inChannel.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+    public void test1() throws Exception {
+        FileInputStream fis = new FileInputStream("/Users/lizehao/IdeaProject/NIOdemo/src/a.txt");
+        FileOutputStream fos = new FileOutputStream("/Users/lizehao/IdeaProject/NIOdemo/src/b.txt");
+        FileChannel inChannel = fis.getChannel();
+        FileChannel outChannel = fos.getChannel();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        while (inChannel.read(byteBuffer) != -1) {
+            byteBuffer.flip();
+            outChannel.write(byteBuffer);
+            byteBuffer.clear();
+        }
+        if (fos != null) {
+            fos.close();
+        }
+        if (fis != null) {
+            fis.close();
+        }
+        if (outChannel != null) {
+            outChannel.close();
+        }
+        if (inChannel != null) {
+            inChannel.close();
         }
     }
 
     @Test
-    public void test2() throws Exception{
+    public void test2() throws Exception {
         FileChannel inChannel = FileChannel.open(Paths.get("/Users/lizehao/IdeaProject/NIOdemo/src/a.txt"), StandardOpenOption.READ);
-        FileChannel outChannel = FileChannel.open(Paths.get("/Users/lizehao/IdeaProject/NIOdemo/src/c.txt"), StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+        FileChannel outChannel = FileChannel.open(Paths.get("/Users/lizehao/IdeaProject/NIOdemo/src/c.txt"), StandardOpenOption.READ,StandardOpenOption.WRITE, StandardOpenOption.CREATE);
 
         MappedByteBuffer inMappedBuf = inChannel.map(FileChannel.MapMode.READ_ONLY, 0, inChannel.size());
-        MappedByteBuffer outMappedBuf = outChannel.map(FileChannel.MapMode.READ_WRITE,0, inChannel.size());
+        MappedByteBuffer outMappedBuf = outChannel.map(FileChannel.MapMode.READ_WRITE, 0, inChannel.size());
 
         byte[] dst = new byte[inMappedBuf.limit()];
         inMappedBuf.get(dst);
@@ -77,7 +51,7 @@ public class TestChannel {
     }
 
     @Test
-    public void test3() throws Exception{
+    public void test3() throws Exception {
         FileChannel inChannel = FileChannel.open(Paths.get("/Users/lizehao/IdeaProject/NIOdemo/src/a.txt"), StandardOpenOption.READ);
         FileChannel outChannel = FileChannel.open(Paths.get("/Users/lizehao/IdeaProject/NIOdemo/src/d.txt"), StandardOpenOption.WRITE, StandardOpenOption.READ);
         inChannel.transferTo(0, inChannel.size(), outChannel);
